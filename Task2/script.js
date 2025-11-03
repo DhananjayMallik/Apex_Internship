4015cdf834d8507cff6401e764686bec2716c01b
DhananjayMallik <dhananjaymallik918@gmail.com>
2025-11-03 22:17:46 +0530

    Final commit for my form submission task

--- Task2/script.js
+++ Task2/script.js
@@ -1,33 +1,60 @@
+// Contact Form
+const btn = document.getElementById("btn-submit");
+const toastRef = document.getElementById("toast");
+const form = document.getElementById("form");
 
-const btn = document.getElementById('btn-submit');
-const toastRef = document.getElementById('toast');
-const form = document.getElementById('form');
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-form.addEventListener('submit',function(e){
-    e.preventDefault();
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const phone = document.getElementById('phone').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-    if(!name || !email || !phone || !password){
-       alert('please fill all the details!!');
-    }
-    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    if(!email.match(emailPattern)){
-        alert('please enter valid email format');
-    }
-    function showMessage(){
-    toastRef.classList.add('show');
-    setTimeout(()=>{
-        toastRef.classList.remove('show');
-    },4000);
-    
-}
+  if (!name || !email || !phone || !password) {
+    alert("please fill all the details!!");
+  }
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!email.match(emailPattern)) {
+    alert("please enter valid email format");
+  }
+  function showMessage() {
+    toastRef.classList.add("show");
+    setTimeout(() => {
+      toastRef.classList.remove("show");
+    }, 4000);
+  }
+  showMessage();
   form.reset();
-btn.addEventListener('click', showMessage);
-});
-
  
+});
+// To Do List
+// get reference 
+const AddTaskBtn = document.getElementById('addTask');
+const TaskInput = document.getElementById('taskInput');
+const TaskList = document.getElementById('taskList');
 
+
+function renderTask(){
+    // get task
+    const task = TaskInput.value.trim();
+    if(!task){
+        alert('First Enter Your Task List');
+        return;
+    }
+    // if the task will occur
+    // render a list
+    const li = document.createElement('li');
+   
+    li.textContent = task;
+    // delete button
+   const delBtn = document.createElement('button');
+      delBtn.textContent = 'Delete';
+      delBtn.style='background-color:red';
+      // apply delete functionality
+      delBtn.onclick = () => li.remove();
+      li.appendChild(delBtn);
+      TaskList.appendChild(li);
+    TaskInput.value = "";
+}
+AddTaskBtn.addEventListener('click',renderTask);
